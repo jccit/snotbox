80e2bad2cd274b1e1ef78cdcd0239b917eaebadf
Josef Citrine <josef@citrine.dev>
2021-07-29 11:38:10 +0100

    Created projects

---
+++ Snotbox/Game.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Snotbox
+{
+    public class Game
+    {
+    }
+}
---
+++ SnotboxRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Snotbox;
+
+namespace SnotboxRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Snotbox.Game game = new Snotbox.Game();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
